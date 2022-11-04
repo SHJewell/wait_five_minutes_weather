614024058e4be93e9fe7b29c6357a9d5c215159d
SHJewell <scott.hjewell@gmail.com>
2022-11-03 19:52:45 -0700

    Added to Jupyter notebooks where processes are developed and tested.
    In statistical_testing.ipynb found some weirdness in STDDEV. Why?

--- analysis_tests.py
+++ analysis_tests.py
@@ -4,23 +4,79 @@ import numpy as np
 import struct
 from netCDF4 import Dataset
 from matplotlib import pyplot as plt
+import pandas as pd
+import os
+import dateutil.parser as parser
+from dataclasses import dataclass
+import datetime
 
+'''
+@dataclass
+class MapLayer:
+    var: str
+    d_start: datetime.date
+    d_end: datetime.date
+    lat: 
+    lon:
+    layer_min:
+    layer_max:
+    mayer_std:
+'''
+
+
+file_directory_external = f"E:\\Documents\\Coding\\wait_five_minutes_weather\\nc_file_directory.csv"
+
+file_directory = pd.read_csv(file_directory_external, index_col=False)
+
+cols = file_directory.columns.tolist()
+
+for folder in file_directory['folder'].unique().tolist():
+
+    this_folder = file_directory[file_directory['folder'] == folder]
+    date_start = datetime.date(1970, 1, 1)
+    date_end = datetime.datetime.now().date()
+
+    for file in this_folder['path']:
+
+        if file[file.find('.'):] != '.nc':
+            continue
+
+        bits = file.split(os.sep)
+
+        if parser.parse(bits[-1][-20:-12]) < date_start:
+            date_start = parser.parse(bits[-1][-20:-12])
+
+        if date_end > parser.parse(bits[-1][-11:-3]):
+            date_end = parser.parse(bits[-1][-11:-3])
+
+        data = Dataset(file)
+        lat = data.variables['lat'][:]
+
+        print(bits[-2], bits[-1])
+
+
+    #print(row['folder'], row['file'].split('_')[0])
+    #print(row, type(row))
 
 # temp_max_file = "E:\\Documents\\Datasets\\Weather Data\\Temp Max\\tasmaxAdjust_day_GFDL-ESM2M_SMHI-DBSrev930-GFD-1981-2010-postproc_rcp45_r1i1p1_20010101-20051231.nc"
 # temp_min_file = "E:\\Documents\\Datasets\\Weather Data\\Temp Min\\tasminAdjust_day_GFDL-CM3_SMHI-DBSrev930-GFD-1981-2010-postproc_rcp45_r1i1p1_20000101-20041231.nc"
-temp_max_file = "C:\Data\Weather Data\Temp Max\\tasmaxAdjust_day_GFDL-ESM2M_SMHI-DBSrev930-GFD-1981-2010-postproc_rcp45_r1i1p1_20010101-20051231.nc"
-temp_min_file = "C:\Data\Weather Data\Temp Min\\tasminAdjust_day_GFDL-CM3_SMHI-DBSrev930-GFD-1981-2010-postproc_rcp45_r1i1p1_20000101-20041231.nc"
+# temp_max_file = "C:\Data\Weather Data\Temp Max\\tasmaxAdjust_day_GFDL-ESM2M_SMHI-DBSrev930-GFD-1981-2010-postproc_rcp45_r1i1p1_20010101-20051231.nc"
+# temp_min_file = "C:\Data\Weather Data\Temp Min\\tasminAdjust_day_GFDL-CM3_SMHI-DBSrev930-GFD-1981-2010-postproc_rcp45_r1i1p1_20000101-20041231.nc"
 
-t_min_grp = Dataset(temp_max_file)
-
-lats = t_min_grp.variables['lat'][:]
-lons = t_min_grp.variables['lon'][:]
-time = t_min_grp.variables['time'][:]
-t_mins = t_min_grp.variables['tasmaxAdjust'][:]
-
-sea_lats = np.argwhere((lats > 46) & (lats < 47))
-sea_lons = np.argwhere((lons > 122) & (lons < 123))
-sea_tmins = t_mins[:, sea_lats, sea_lons].data
+# t_min_grp = Dataset(temp_max_file)
+#
+# lats = t_min_grp.variables['lat'][:]
+# lons = t_min_grp.variables['lon'][:]
+# time = t_min_grp.variables['time'][:]
+# t_mins = t_min_grp.variables['tasmaxAdjust'][:]
+#
+# sea_lats = np.argwhere((lats > 46) & (lats < 47))
+# sea_lons = np.argwhere((lons > 122) & (lons < 123))
+# sea_tmins = t_mins[:, sea_lats, sea_lons].data
+#
+# type(sea_lats)
+#
+# def gen_processes(path):
 
 
 # N = len(sea_tmins)
